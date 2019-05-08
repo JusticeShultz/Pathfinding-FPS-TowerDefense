e49958e908fd02d5fc1e5e34d7a51393e91bf80e
Justice Shultz <shultzjc1@gmail.com>
2019-05-08 16:34:50 -0700

    Added sawblade buildable

--- Assets/Imported/Spin.cs
+++ Assets/Imported/Spin.cs
@@ -1,5 +1,6 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
@@ -9,6 +10,6 @@ public class Spin : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		transform.Rotate(axis, speed*Time.deltaTime);
+		transform.Rotate(axis, speed * Time.deltaTime);
 	}
 }
---
+++ Assets/Turrets/DamageRegion.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageRegion : MonoBehaviour
+{
+    public float Damage;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            other.GetComponent<AI>().Health -= Damage;
+        }
+    }
+}
